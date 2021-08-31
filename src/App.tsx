92d14b42167dbb2edf8f461d37e45850312d286b
wangqiaoling <wangqiaoling@MacintoshdeMacBook-Pro.local>
2021-08-31 17:10:30 +0800

    feat: 添加git hook 配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,19 @@
 import React from 'react';
-import styles from './App.module.css'
-import { Header, Footer, Carousel, SideMenu, ProductCollection, BusinessPartners } from './components'
-import { Row, Col, Typography } from 'antd'
-import { productList1, productList2, productList3 } from './mockup'
-import sideImage1 from './assets/images/sider_2019_12-09.png'
-import sideImage2 from './assets/images/sider_2019_02-04.png'
-import sideImage3 from './assets/images/sider_2019_02-04-2.png'
-
+import { HomePage } from './pages'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 function App() {
   return (
-    <div className={styles.App}>
-      <Header />
-      {/* 页面内容 content */}
-      <div className={styles['page-content']}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="warning">
-              爆款推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage1}
-          products={productList1}
-        />
-
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="danger">
-              新品上市
-            </Typography.Title>
-          }
-          sideImage={sideImage2}
-          products={productList2}
-        />
-
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="success">
-              国内游推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage3}
-          products={productList3}
-        />
-
-        <BusinessPartners />
-      </div>
-      <Footer />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/signIn" render={() => <h1>登录页面</h1>}></Route>
+        <Route
+          render={() => {
+            ;<h1>404</h1>
+          }}
+        ></Route>
+      </Switch>
+    </Router>
   )
 }
 
